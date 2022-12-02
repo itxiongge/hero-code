08f2d408ee4c847a7233fa7c43741df1228d6dea
Vincent-IT雄哥 <85910652+itxiongge@users.noreply.github.com>
2022-12-02 13:20:17 +0800

    Update SpringConfiguration.java

--- springboot-demos/springboot03-conditional-annotation-example/src/main/java/com/hero/config/SpringConfiguration.java
+++ springboot-demos/springboot03-conditional-annotation-example/src/main/java/com/hero/config/SpringConfiguration.java
@@ -17,7 +17,7 @@ public class SpringConfiguration {
      *   注解作用：当我的classpath路径下，存在Tomcat类，则执行本方法（导入dog）
      *   可以放到：可以放到类上，可以放到方法上
      * @ConditionalOnProperty
-     *   注解作用：当配置文件中有spring.itcast.enable配置值，且配置值为true时，条件生效
+     *   注解作用：当配置文件中有spring.herobatis.enable配置值，且配置值为true时，条件生效
      *   name属性，设置配置key
      *   havingValue属性，设置配置的默认值
      *   matchIfMissing属性，设置配置value匹配的值，如果匹配条件生效，如果不匹配，那么不生效
